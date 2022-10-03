3769c6c57a6eb0a54b5385cb284f67ac15f08bda
Roman <popov.roman.e@gmail.com>
2022-10-03 23:50:04 +0500

    исправляет module2-task1

--- js/main.js
+++ js/main.js
@@ -1,19 +1,15 @@
-// let backgroundBody = document.querySelector('body');
-// backgroundBody.classList.add('tomato');
-// backgroundBody.classList.remove('tomato');
-// проверка: подключил main.js к странице
-
 // module2-task1
-const inputText = 'hello, Guys';
+const inputText = 'hello';
 let requiredLength = 5;
 const getRequiredLength = (inputText,requiredLength) => {
-  if (inputText.length===requiredLength){
-    return console.log('true');
+  if (inputText.length<=requiredLength){
+    return true;
   }
-  return console.log('false');
+  return false;
 }
+console.log(getRequiredLength(inputText,requiredLength));
 
-getRequiredLength(inputText,requiredLength)
+getRequiredLength(inputText,requiredLength);
 
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 let getRandomIntInclusive = (min, max) => {
@@ -28,14 +24,8 @@ let getRandomIntInclusive = (min, max) => {
   }
 
   min = Math.ceil(min); //roundUp
-  console.log(min,'roundUp min');
-
   max = Math.floor(max); //roundDown
-  console.log(max,'roundDown max');
-
   let random = Math.random(); //random from 0 to 1 (floor)
-  console.log(random, 'random from 0 to 1 (floor)');
-
   return Math.floor(random * max - random * min + random) + min; //Максимум и минимум включаются
 }
-console.log(getRandomIntInclusive(4,8), 'random int from area', 'Math.floor(random * max - random * min + random) + min');
+console.log(getRandomIntInclusive(4,8));
