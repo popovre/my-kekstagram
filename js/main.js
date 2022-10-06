8121d61316403afcfd9bb798ebe1948d4cb72c3e
popovre <popov.roman.e@gmail.com>
2022-10-06 15:16:24 +0500

    made photo gallery

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
-const inputText = 'hello';
-const requiredLength = 5;
 const getRequiredLength = (text,length) => {
   if (text.length <= length){
     return true;
   }
   return false;
 };
-getRequiredLength(inputText,requiredLength);
+getRequiredLength('hi',5);
 
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 const getRandomIntInclusive = (min, max) => {
@@ -20,9 +18,59 @@ const getRandomIntInclusive = (min, max) => {
     max = reroll;
   }
 
-  min = Math.ceil(min); //roundUp
-  max = Math.floor(max); //roundDown
-  const random = Math.random(); //random from 0 to 1 (floor)
-  return Math.floor(random * max - random * min + random) + min; //Максимум и минимум включаются
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const random = Math.random();
+  return Math.floor(random * max - random * min + random) + min;
 };
-getRandomIntInclusive(4,8);
+
+const GALLERY_LENGTH = 25;
+const DESCRIPTIONS = [
+  'спортзал',
+  'видеокарта',
+  'компьютер',
+  'солнышко',
+  'древесный кенгуру',
+  'хомячки',
+  'мыши',
+  'поляна',
+  'деревце',
+  'калина',
+  'верблюд',
+  'котик',
+  'машины',
+  'горы',
+  'мотоциклы',
+  'стол',
+  'розетка',
+  'фронтенд разработка',
+  'бэкенд разработчик',
+  'язык С или С++',
+  'микроконтроллер',
+  'STM32',
+  'Induction motor',
+  'ACED2021',
+  'IEEE',
+];
+
+const getPhotoDescription = function(index){
+  return {
+    id: index,
+    url: `photos/${index}.jpg`,
+    description: DESCRIPTIONS[index - 1],
+    likes: getRandomIntInclusive(15,200),
+    comments: getRandomIntInclusive(0,200)
+  };
+};
+
+const getPhotoGallery = (photoDescription) => {
+  const gallery = [];
+  for(let i = 1;i <= GALLERY_LENGTH;i++){
+    gallery.push(photoDescription(i));
+  }
+  return gallery;
+};
+
+const photoGallery = getPhotoGallery(getPhotoDescription);
+
+console.log(photoGallery);
