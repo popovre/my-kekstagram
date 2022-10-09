719a52d17420278a7f1b10a5b8e022d13ff9b023
Roman <popov.roman.e@gmail.com>
2022-10-09 22:21:57 +0500

    ready module4-task1 + linter

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ getRequiredLength('hi',5);
 
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 const getRandomIntInclusive = (min, max) => {
-  if(min < 0 || max < 0){
+  if(min < 0 || max < 0 || min === max){
     return NaN;
   }
 
@@ -54,66 +54,15 @@ const DESCRIPTIONS = [
   'IEEE',
 ];
 
-//первый вариант решения с 58 строки по 80
 const getPhotoDescription = function(index){
-  const photoKeys = [
-    'id',
-    'url',
-    'description',
-    'likes',
-    'comments'
-  ];
-  const photoValues = [
-    index + 1,
-    `photos/${index + 1}.jpg`,
-    DESCRIPTIONS[index],
-    getRandomIntInclusive(15,200),
-    getRandomIntInclusive(0,200)
-  ];
-
-  const photoDescription = {};
-  photoKeys.forEach((value,ind) => {
-    photoDescription[value] = photoValues[ind];
-  });
-
-  return photoDescription;
+  return {
+    id: index + 1,
+    url: `photos/${index + 1}.jpg`,
+    description: DESCRIPTIONS[index],
+    likes: getRandomIntInclusive(15,200),
+    comments: getRandomIntInclusive(0,200)
+  };
 };
 
-const galleryPhoto = Array.from({ length:GALLERY_LENGTH }, (v, k) => getPhotoDescription(k));
-console.log(galleryPhoto);
+const galleryPhotos = Array.from({ length:GALLERY_LENGTH }, (value, ind) => getPhotoDescription(ind));
 
-// второй вариант решения с 86 строки по 97
-// const getPhotoDescription = function(index){
-//   return {
-//     id: index + 1,
-//     url: `photos/${index + 1}.jpg`,
-//     description: DESCRIPTIONS[index],
-//     likes: getRandomIntInclusive(15,200),
-//     comments: getRandomIntInclusive(0,200)
-//   };
-// };
-
-// const galleryPhoto = Array.from({ length:GALLERY_LENGTH }, (v, k) => getPhotoDescription(k));
-// console.log(galleryPhoto);
-
-
-// третий вариант решения с 101 строки по 119
-// const getPhotoDescription = function(index){
-//   return {
-//     id: index + 1,
-//     url: `photos/${index + 1}.jpg`,
-//     description: DESCRIPTIONS[index],
-//     likes: getRandomIntInclusive(15,200),
-//     comments: getRandomIntInclusive(0,200)
-//   };
-// };
-
-// const getPhotoGallery = (photoDescription) => {
-//   const gallery = [];
-//   for(let i = 1;i <= GALLERY_LENGTH;i++){
-//     gallery.push(photoDescription(i));
-//   }
-//   return gallery;
-// };
-
-// const photoGallery = getPhotoGallery(getPhotoDescription);
