e3f5d2046b28ff303d377fbacda6c718f36a2e96
popovre <popov.roman.e@gmail.com>
2022-10-26 16:35:19 +0500

    пытается добавить доп. валидацию форме

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,7 +1,27 @@
-const uploadForm = document.querySelector('.img-upload__form');
+import {MINDESCRIPTIONLENGT,MAXDESCRIPTIONLENGT} from './constants.js';
 
+const uploadForm = document.querySelector('.img-upload__form');
 const pristine = new Pristine(uploadForm, {
-  classTo: 'text__container',
-  errorTextParent: 'text__container',
-  errorTextClass: 'text__description-error',
+  classTo: 'text',
+  errorTextParent: 'text',
+  errorTextClass: 'text__description--error',
+  errorClass: 'text__description--invalid',
+  successClass: 'text__description--valid',
+  errorTextTag: 'div',
+});
+
+const validateDescriptionText = (value) => value.length >= MINDESCRIPTIONLENGT && value.length <= MAXDESCRIPTIONLENGT;
+
+pristine.addValidator(
+  uploadForm.querySelector('.text__description'),
+  validateDescriptionText,
+  'От 20 до 140 символов, зачем я здесь?'
+);
+
+uploadForm.addEventListener('submit', (evt) => {
+  const isValid = pristine.validate();
+
+  if(!isValid) {
+    evt.preventDefault();
+  }
 });
