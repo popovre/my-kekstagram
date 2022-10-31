4dbb3dcb7f864697e95da5b871fb206924af0f66
popovre <popov.roman.e@gmail.com>
2022-10-31 13:35:20 +0500

    добавляет план действий

--- js/form-activities.js
+++ js/form-activities.js
@@ -3,7 +3,7 @@ const scaleSmallerButton = scaleFieldset.querySelector('.scale__control--smaller
 const scaleBiggerButton = scaleFieldset.querySelector('.scale__control--bigger');
 const scaleValueInput = scaleFieldset.querySelector('.scale__control--value');
 // console.log(scaleValue);
-
+// давай сделаем обработчик на филдсет scaleFieldset, по всплытию будет один обработчик на 2 кнопки, если плюс, то scale будет плюс, если минус, то scale будет плюс.
 const onScaleSmallerButton = () => {
   let scaleValue = Number(scaleValueInput.value.delOneLast());
   scaleValue = scaleValue - 25;
