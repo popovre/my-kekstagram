65b54ec278ff9a7903a904e110d346ffbeaaa03e
popovre <popov.roman.e@gmail.com>
2022-11-02 16:46:44 +0500

    исправляет баги эффектов

--- js/form-activities.js
+++ js/form-activities.js
@@ -21,6 +21,7 @@ const isDefault = () => chosenEffect === DEFAULT;
 const previewDefault = (element, scaleDefault) => {
   previewImage.className = '';
   previewImage.style.transform = `scale(${getIntToFloat(scaleDefault) / 100})`;
+  previewImage.style.filter = 'none';
 };
 
 const onScaleButton = (evt) => {
@@ -67,24 +68,23 @@ const onEffectsRadio = (evt) => {
   const previewImageClassList = Array.from(previewImage.classList);
   removeCoincidenceClass(previewImage, previewImageClassList, effectClasses, coincidenceStyle);
   chosenEffect = EFFECTS.find((effect) => effect.name === evt.target.value);
-  console.log(chosenEffect);
   getSliderUpdate();
   previewImage.classList.add(`effects__preview--${evt.target.value}`);
 };
 
 const onUpdateSliderEffect = () => {
   previewImage.style.filter = 'none';
-  previewImage.style.filter = 'none';
-  //допилить
-  previewImage.className = '';
-  //
+  const previewImageClassList = Array.from(previewImage.classList);
+  removeCoincidenceClass(previewImage, previewImageClassList, effectClasses, coincidenceStyle);
   sliderEffectInput.value = '';
+
   if (isDefault()) {
     return;
   }
 
   const sliderValue = sliderEffectWrapper.noUiSlider.get();
   previewImage.style.filter = `${chosenEffect.style}(${sliderValue}${chosenEffect.unit})`;
+  previewImage.classList.add(`effects__preview--${chosenEffect.name}`);
   sliderEffectInput.value = sliderValue;
 };
 
@@ -97,9 +97,15 @@ noUiSlider.create(sliderEffectWrapper, {
   step: DEFAULT.step,
   connect: 'lower',
 });
+
+const resetEffect = () => {
+  chosenEffect = DEFAULT;
+  getSliderUpdate();
+}
+
 getSliderUpdate();
 
 sliderEffectWrapper.noUiSlider.on('update', onUpdateSliderEffect);
 
-export {scaleFieldset, onScaleButton, effectsList, onEffectsRadio, previewImage, previewDefault, MAXSCALE};
+export {scaleFieldset, onScaleButton, effectsList, onEffectsRadio, previewImage, previewDefault, MAXSCALE, resetEffect};
 
--- js/form-upload.js
+++ js/form-upload.js
@@ -1,6 +1,6 @@
 import {isEscapeKey} from './utils.js';
 import {uploadForm, onUploadFormSubmit} from './form-validation.js';
-import {scaleFieldset, onScaleButton, effectsList, onEffectsRadio, previewImage, previewDefault, MAXSCALE} from './form-activities.js';
+import {scaleFieldset, onScaleButton, effectsList, onEffectsRadio, previewImage, previewDefault, MAXSCALE, resetEffect} from './form-activities.js';
 
 const body = document.querySelector('body');
 const uploadFile = body.querySelector('#upload-file');
@@ -23,6 +23,7 @@ const openUploadModal = () => {
   scaleFieldset.addEventListener('click', onScaleButton);
   effectsList.addEventListener('input',onEffectsRadio);
   previewDefault(previewImage, MAXSCALE);
+  resetEffect();
 };
 
 const closeUploadModal = () => {
