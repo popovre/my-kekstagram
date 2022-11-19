6ea1d3484adcf23bd0df2a3d25f830f952263d93
Roman <popov.roman.e@gmail.com>
2022-11-20 00:15:39 +0500

    убирает лишнюю функцию очистки класса

--- js/utils.js
+++ js/utils.js
@@ -1,17 +1,3 @@
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 export {isEscapeKey};
-
-const removeCoincidenceClass = (nodeElement, nodeElementArray, coincidenceArray, coincidenceClass) => {
-  if (nodeElementArray.length > 0) {
-    for (let i = 0; i < nodeElementArray.length; i++) {
-      coincidenceClass = coincidenceArray.find((constantClass) => constantClass === nodeElementArray[i]);
-      if (coincidenceClass !== undefined) {
-        nodeElement.classList.remove(coincidenceClass);
-        break;
-      }
-    }
-  }
-};
-
-export {removeCoincidenceClass};
