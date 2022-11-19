84114795c28aabbfb4ca9c82f093f90086be48b3
Roman <popov.roman.e@gmail.com>
2022-11-19 21:08:53 +0500

    удаляет неиспольхуемые функции

--- js/utils.js
+++ js/utils.js
@@ -1,29 +1,3 @@
-const getRequiredLength = (text,length) => {
-  if (text.length <= length){
-    return true;
-  }
-  return false;
-};
-
-const getRandomIntInclusive = (min, max) => {
-  if(min < 0 || max < 0 || min === max){
-    return NaN;
-  }
-
-  if(min > max){
-    const reroll = min;
-    min = max;
-    max = reroll;
-  }
-
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  const random = Math.random();
-  return Math.floor(random * max - random * min + random) + min;
-};
-
-export {getRequiredLength,getRandomIntInclusive};
-
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 export {isEscapeKey};
