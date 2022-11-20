3d2c66a406844ebd111d94cc5301d0d588145824
Roman <popov.roman.e@gmail.com>
2022-11-20 10:20:07 +0500

    убрал избыточкую проверку массива

--- js/utils.js
+++ js/utils.js
@@ -3,13 +3,11 @@ const isEscapeKey = (evt) => evt.key === 'Escape';
 export {isEscapeKey};
 
 const removeCoincidenceClass = (nodeElement, nodeElementArray, coincidenceArray, coincidenceClass) => {
-  if (nodeElementArray.length > 0) {
-    for (let i = 0; i < nodeElementArray.length; i++) {
-      coincidenceClass = coincidenceArray.find((constantClass) => constantClass === nodeElementArray[i]);
-      if (coincidenceClass !== undefined) {
-        nodeElement.classList.remove(coincidenceClass);
-        break;
-      }
+  for (let i = 0; i < nodeElementArray.length; i++) {
+    coincidenceClass = coincidenceArray.find((constantClass) => constantClass === nodeElementArray[i]);
+    if (coincidenceClass !== undefined) {
+      nodeElement.classList.remove(coincidenceClass);
+      break;
     }
   }
 };
